bd0e8cf68b7116c0ab294b8ff42150d52d46a43f
Bishop Bader <bflatbader@users.noreply.github.com>
2019-12-09 14:30:36 -0500

    chained two then promises to make two separate scrapes, then redirect to homepage, displaying sorted articles

--- config/routes.js
+++ config/routes.js
@@ -28,7 +28,7 @@ module.exports = function(router) {
     // A GET route for scraping HuffPo
     router.get("/scrape", function(req, res){
         // Grab the body of the html with axios
-        axios.get("https://search.huffpost.com/search?p=coffee+%26+crime").then(function(response) { 
+        axios.get("https://search.huffpost.com/search?p=murder").then(function(response) { 
             // Then, we load that into cheerio and save it to $
             var $ = cheerio.load(response.data);
 
@@ -57,14 +57,57 @@ module.exports = function(router) {
 
                 console.log(result);
                 db.Headline.create(result)
-                    .then(function(dbHeadline) {
-                        // View the added result in the console
-                        console.log(dbHeadline);
-                    })
-                    .catch(function(err) {
-                        // If an error occurred, log it
-                        console.log(err);
-                    });
+                        .then(function(dbHeadline) {
+                            // View the added result in the console
+                            console.log(dbHeadline);
+                        })
+                        .catch(function(err) {
+                            // If an error occurred, log it
+                            console.log(err);
+                        });
+            });
+
+        }).then(function (){
+            // Grab the body of the html with axios
+            axios.get("https://search.huffpost.com/search?p=coffee").then(function(response) { 
+                // Then, we load that into cheerio and save it to $
+                var $ = cheerio.load(response.data);
+
+                $(".bt-dbdbdb").each(function(i, element) {
+                    result = {};
+
+                    result.headline = $(this)
+                        .children("a")
+                        .attr("title").trim();
+                    result.link = $(this)
+                        .children("a")
+                        .attr("href");
+                    result.imgUrl = $(this)
+                        .children("a")
+                        .children("img")
+                        .attr("src");
+                    result.summary = $(this)
+                        .children("p")
+                        .text();
+                    result.date = $(this)
+                        .children("div")
+                        .children("p")
+                        .children("span:nth-child(2)")
+                        .text()
+                    result.displayDate = moment(result.date).format('MMMM Do YYYY');
+
+                    console.log(result);
+                    db.Headline.create(result)
+                        .then(function(dbHeadline) {
+                            // View the added result in the console
+                            console.log(dbHeadline);
+                        })
+                        .catch(function(err) {
+                            // If an error occurred, log it
+                            console.log(err);
+                        });
+
+                });
             });
 
             // Redirect to the home page
